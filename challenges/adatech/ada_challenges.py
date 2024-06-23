896d221ce494a4f8520494a5afec65690cd4dd6a
idelfrides <eng.idelfridesjorge@gmailcom>
2024-06-23 00:16:58 -0300

    feat: solve one new ada tech challenge

--- challenges/adatech/ada_challenges.py
+++ challenges/adatech/ada_challenges.py
@@ -1,21 +1,12 @@
 #!bin/env/python3
 
 from LIBS.manager import end_app_execution
+from LIBS.ijdev_helper import special_chars
 
+
+# --------------------------------------------------
 
 # CHALLENGE 1 : BALANCEAMNTO DE COLCHETES EM UMA EXPRESSÃO
-# Data uma string de expressao exp, escreva um programa para examinar se os pares e as ordens de "{" , "}", "(", ")", "[", "]" estao corretos na expressao dada .
-
-# Exemplos de entradas: 
-#  2*(3+4+5*[2+3)] --> ERRADO  |  2*(3+4+5*[2+3]) --> CORRETO
-#  [2*(3+4+5*[2+3]/(2+(3+(7+5)*9)*6)*[5+8]+17)+2] --> CORRETO
-
-# SOLUCAO: uso de pilhas
-# 1 - empilhar caracteres de abertura 
-# 2 - ao identificar caractere de fechamento:
-    # * desempilhar e verificar se coincide com abertura 
-# 3 - ao final a pilha deve estar vazia 
-
 def bracket_balancing(exp: str) -> None:
     """#### BALANCEAMNTO DE COLCHETES EM UMA EXPRESSÃO
         Data uma string de expressao exp, escreva um programa para examinar se os pares e as ordens de "{" , "}", "(", ")", "[", "]" estao corretos na expressao dada .
@@ -39,9 +30,9 @@ def bracket_balancing(exp: str) -> None:
         exp (str): entering expression
     """
     
-    print(' BALANCEAMNTO DE COLCHETES EM UMA EXPRESSÃO')
-    OPEN_CHARACTERS = ('(', '[', '{')
-    CLOSE_CHARACTERS = (')', ']', '}')
+    print(" BALANCEAMNTO DE COLCHETES EM UMA EXPRESSÃO")
+    OPEN_CHARACTERS = ("(", "[", "{")
+    CLOSE_CHARACTERS = (")", "]", "}")
     
     pill_array = list()
     
@@ -52,16 +43,6 @@ def bracket_balancing(exp: str) -> None:
     
     return
 
-
-# -------------------------------------------------------------------------------
-
-# CHALLENGE 2 -  Revertendo os primeiros k elementos de uma fila
-
-# Dado um inteiro k e uma fila de inteiros, a tarefa é inverter a ordem dos primeiros k elemetos da fila, deixand os demais elementos na mesma ordem relativa .
-
-# Exemplos:
-    #  [1, 2, 3, 4, 5] -> k=2 --> [2, 1, 3, 4, 5] 
-    #  [1, 2, 3, 4, 5] -> k=4 --> [4, 3, 2, 1, 5] 
     
 def revert_first_k_elements(k: int, elements: list[int]) -> None:
     """### Revert firt k elements
@@ -88,7 +69,7 @@ def revert_first_k_elements(k: int, elements: list[int]) -> None:
     
     print(f"\n K: {k}\n ELEMENTS: {elements}\n THE FINAL RESULT IS:  {result_list}")
     
-    print('\n\n')    
+    print("\n\n")    
     return
 
 
@@ -135,33 +116,75 @@ def run_revert_first_k_elements():
 
     
 def run_greatest_value_in_each_tree_level():
-    print('\n\n\n ENTER THE HIGHT OF YOUR BINARY TREE  \n\n')
+    print("\n\n\n ENTER THE HIGHT OF YOUR BINARY TREE  \n\n")
     
     tree_hight = int(input("ENTER A VALUE HERE :::>  ").strip())
     btree = []
     
     for N in range(tree_hight):
-        print(f'\n ------- VALUES FOR LEVEL {N} --------\n')
+        print(f"\n ------- VALUES FOR LEVEL {N} --------\n")
         btree.append(list(map(int, input("ENTER E SEQUENCE OF VALUES :::>>  ").rstrip().split())))
 
     print(btree)
     result = greatest_value_in_each_tree_level(tree_elements=btree)
     
-    print(f' \n\n THE RESULT IS >>>>>>  {result}')  
-    print('\n\n RESULT DETAIL \n\n')
+    print(f" \n\n THE RESULT IS >>>>>>  {result}")  
+    print("\n\n RESULT DETAIL \n\n")
     for n, g in enumerate(result):
-        print(f'LEVEL {n}  |  GREATEST {g}')
+        print(f"LEVEL {n}  |  GREATEST {g}")
         
     return
+
+def run_verifica_senha():
+    
+    # pwd = "senhateste#57@9"  # [2, 3]
+    pwd_in = input("ENTER THE PASSWORD ::>>  ").strip()  
+    print(verifica_senha(pwd_in)) 
+    
+    return
+    
+
+# CHALLENGE 4 - Verifica caracteres especiais e números presentes em senha
+def verifica_senha(senha: str) -> list[int]:
+    """### Verifica caracteres especiais e números presentes em senha
+    
+    Ao desenvolver um produto de segurança de senha em uma empresa, criou-se uma regra que as senhas devem possuir números e caracteres especiais. Após um ataque cibernético, decidiram fazer um estudo para verificar se aumentar a segurança da senha, de forma que existisse mais de um número e caracter especial seria efetivo para aumentar a segurança. Pensando nisso, implemente uma função que realize a contagem de quantos caracteres especiais e números existem na senha.
+
+    Utilize o corpo de função definido abaixo e retorne uma lista de dois elementos, em que o primeiro elemento é a quantidade de caracteres especiais e o seguindo elemento é a quantidade de números existentes na senha.
+
+    def verifica_senha(string senha):
+
+    Args:
+        senha (str): real password string
+
+    Returns:
+        list[int]: a list of quantity of special chars and numbers
+    """
+    
+    result = []
+    special_char_count = 0
+    numeric_count = 0
+    
+    for c in senha:
+        if str(c).isnumeric():
+            numeric_count += 1
+        elif c in special_chars:
+            special_char_count += 1
+    
+    result.append(special_char_count)
+    result.append(numeric_count)
+    
+    return result
 
 
 def run_ada_challenges():
     menu_options = [
-        'BALANCEAMNTO DE COLCHETES EM UMA EXPRESSÃO',
-        'Revertendo os primeiros k elementos de uma fila',
-        'maior valor de cada nivel de uma arvore binaria'
+        "BALANCEAMNTO DE COLCHETES EM UMA EXPRESSÃO",
+        "Revertendo os primeiros k elementos de uma fila",
+        "maior valor de cada nivel de uma arvore binaria",
+        "verificar senha"
     ]
-    print('\n\n')
+    print("\n\n")
     print(f"------------ MENU OF OPTIOS ------------\n")
     for i, op in enumerate(menu_options, start=1):
         print(f"{i} -> {op.upper()}")
@@ -186,6 +209,8 @@ def run_ada_challenges():
             run_revert_first_k_elements_v2()
         case 3:
             run_greatest_value_in_each_tree_level()
+        case 4:
+            run_verifica_senha()
         case 0:
             end_app_execution()
     
