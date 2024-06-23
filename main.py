09ea8b7388ca20203df04bcd0c48ffb9822d47de
idelfrides <eng.idelfridesjorge@gmailcom>
2024-06-23 00:16:00 -0300

    feat: update main

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from challenges.adatech import ada_challenges
 
 def run_challenge():
   ada_challenges.run_ada_challenges()
-
+  return
 
 
 
 if __name__ == "__main__":
-    run_challenge()
+  run_challenge()
